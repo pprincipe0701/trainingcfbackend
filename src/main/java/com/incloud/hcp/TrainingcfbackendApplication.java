f02b335d13e9f9e54907bb4696476037cdf2a31a
pprincipe.cons@gmail.com <TheBest01$$>
2022-04-12 17:26:36 -0500

    subida proyecto

---
+++ src/main/java/com/incloud/hcp/TrainingcfbackendApplication.java
@@ -0,0 +1,13 @@
+package com.incloud.hcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingcfbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingcfbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incloud/hcp/TrainingcfbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incloud.hcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingcfbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
